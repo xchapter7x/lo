eeed35da1ba228f4e044df4a71d105c00edc84de
John Calabrese <jcalabrese@pivotal.io>
2016-08-04 19:40:00 -0400

    using an interface instead of type for G so we can fake log output in tests

--- init.go
+++ init.go
@@ -11,8 +11,29 @@ const (
 )
 
 var (
-	G *logging.Logger
+	G logger
 )
+
+type Logger interface {
+	Critical(args ...interface{})
+	Criticalf(format string, args ...interface{})
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	IsEnabledFor(level Level) bool
+	Notice(args ...interface{})
+	Noticef(format string, args ...interface{})
+	Panic(args ...interface{})
+	Panicf(format string, args ...interface{})
+	SetBackend(backend LeveledBackend)
+	Warning(args ...interface{})
+	Warningf(format string, args ...interface{})
+}
 
 func init() {
 
